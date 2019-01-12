62e7ff75cb8ed84d1364c12a4dd339bb814afcc3
talieScript <taliesin.bowes@hotmail.co.uk>
2019-01-12 11:22:13 +0000

    Created a basic template

---
+++ caseConverter.js
@@ -0,0 +1,22 @@
+$('.ui.radio.checkbox')
+  .checkbox()
+;
+
+
+// function kebabToCamel(str){
+// 	var c = [];
+// 	var sep = str.split("-");
+// 	for(var i=0;i<sep.length;i++){
+// 		c.push(sep[i][0].toUpperCase()+sep[i].slice(1));
+// 	}
+// 	var capitalized = c.join("-");
+//   capitalized.charAt(0).toLowerCase() +     capitalized.slice(1);
+// 	var re = /-/gi;
+//   var converted = capitalized.replace(re, '');
+//   return converted;
+// }
+
+//   function kebabToSnake(str){
+//   	var converted = str.replace(/-/g, "_");
+//   	return converted;
+//   };
