56b24dc2e22acf9aae5febb63ae03c9755ba8641
talieScript <taliesin.bowes@hotmail.co.uk>
2019-01-17 02:00:15 +0000

    added kebab to camel and kebab to snake

--- caseConverter.js
+++ caseConverter.js
@@ -1,22 +1,49 @@
 $('.ui.radio.checkbox')
   .checkbox()
-;
 
 
-// function kebabToCamel(str){
-// 	var c = [];
-// 	var sep = str.split("-");
-// 	for(var i=0;i<sep.length;i++){
-// 		c.push(sep[i][0].toUpperCase()+sep[i].slice(1));
-// 	}
-// 	var capitalized = c.join("-");
-//   capitalized.charAt(0).toLowerCase() +     capitalized.slice(1);
-// 	var re = /-/gi;
-//   var converted = capitalized.replace(re, '');
-//   return converted;
-// }
+$("#convert").click(function(){
+  var radioValInput = $("input[name='input']:checked").val()
+  var radioValOutput = $("input[name='output']:checked").val()
+  var textInput = $("#textInput").val()
+  var textOutput = $("#textOutput").val()
+  if(radioValInput==undefined || radioValOutput==undefined){
+    alert("Please select input and output")
+  } else if($("#textInput").val() == ""){
+    alert("Please enter some input text ")
+  } else{
+    if(radioValInput === "kebabInput"){
+      if(radioValOutput === "camelOutput"){
+        $("#textOutput").val(kebabToCamel(textInput))
+      }
+      else if(radioValOutput === "snakeOutput"){
+        $("#textOutput").val(kebabToSnake($("#textInput").val()))
+      }
+    } 
+  }   
+})
 
-//   function kebabToSnake(str){
-//   	var converted = str.replace(/-/g, "_");
-//   	return converted;
-//   };
+
+function kebabToCamel(str){
+	var c = [];
+	var sep = str.split("-");
+	for(var i=0;i<sep.length;i++){
+		c.push(sep[i][0].toUpperCase()+sep[i].slice(1));
+	}
+	var campt = c.join("-");
+  var re = /-/gi;
+  campt = campt.replace(re, '');
+  campt = campt.split(' ');
+  console.log(campt.length)
+  var converted = [];
+  for(var i = 0; i<campt.length-1; i++){
+    campt[i] = campt[i].replace(campt[i][0], campt[i][0].toLowerCase())
+    converted.push(campt[i]);
+  }
+  return converted.join(' ');
+}
+
+function kebabToSnake(str){
+  var converted = str.replace(/-/g, "_");
+  return converted;
+};
